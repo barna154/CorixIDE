739135eba0de38c7b6f8607e73f131091f31e1e2
barna154 <barnax.craft@gmail.com>
2026-03-10 20:23:25 +0100

    update

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+
+        window.setTitle("Első ablak");
+        window.setSize(400,300);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.setVisible(true);
+    }
+}

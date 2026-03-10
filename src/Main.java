7a2c0dd1eb16aab133b7e9c5fbf3a4d16debf9ba
barna154 <barnax.craft@gmail.com>
2026-03-10 21:04:27 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,33 @@ import java.util.Scanner;
 import javax.swing.JFrame;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
- 
-        //SCANNER a stringekhez
-        public static String getValue(String key, Scanner sc) {
-              sc = new Scanner(new File("../lang/lang.json")); 
-                String value = "";
-              while (sc.hasNextLine()) {
-                    String line = sc.nextLine().trim();
-                         if (line.startsWith("\"" + key + "\"")) {
-                              int firstQuote = line.indexOf("\"", key.length() + 2);
-                              int secondQuote = line.indexOf("\"", firstQuote + 1);
-                               value = line.substring(firstQuote + 1, secondQuote);
-                        break;
-                         }
-                  }
-                    sc.close();
-                return value;
-            
+
+    //LANGUAGE SCANNER
+    public static String getValue(String key) throws Exception {
+        Scanner sc = new Scanner(new File("../lang/lang.json")); 
+        String value = "";
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine().trim();
+            if (line.startsWith("\"" + key + "\"")) {
+                int firstQuote = line.indexOf("\"", key.length() + 2);
+                int secondQuote = line.indexOf("\"", firstQuote + 1);
+                value = line.substring(firstQuote + 1, secondQuote);
+                break;
             }
+        }
+        sc.close();
+        return value;
+    }
 
-        //STRINGLIST
-            String title = getValue("Title", null);
+    public static void main(String[] args) throws Exception {
 
-   
+        // STRINGLIST
+        String title = getValue("Title");
+
+     
 
 
+     
         JFrame window = new JFrame();
         window.setTitle(title);
         window.setSize(400, 300);

6a5cdf88e2dbc34648cf6206e54edbce959a49df
barna154 <barnax.craft@gmail.com>
2026-03-10 21:30:43 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -45,6 +45,7 @@ public class Main {
         {
                 JPanel Menu = new JPanel();
                 Menu.setBackground(Color.LIGHT_GRAY);
+                Manu.setSize(1080, 100);
 
                 ImageIcon icon = new ImageIcon("../gui/logo.png");
                 Image img = icon.getImage(); 

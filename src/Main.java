994968f2c3583d44161dcaea59a5a97fd7da60c8
barna154 <barnax.craft@gmail.com>
2026-03-10 21:49:48 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -48,7 +48,7 @@ public class Main {
 
                 final int[] mouseOffset = new int[2];
                 JPanel Menu = new JPanel();
-                Menu.setBackground(Color.GRAY);
+                Menu.setBackground(Color.BLACK);
                 Menu.setPreferredSize(new Dimension(1080, 30)); // magasság 300px
                 Menu.setLayout(new FlowLayout(FlowLayout.LEFT));
 

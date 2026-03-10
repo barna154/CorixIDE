7f147b0a7e39352e5f6c0e6b53b2704d70b6d529
barna154 <barnax.craft@gmail.com>
2026-03-10 20:45:26 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Main {
+     static String json = Paths.get("lang/lang.json");
+
     public static void main(String[] args) {
+
+        JSONObject obj = new JSONObject(json);
+        String Title = obj.getString("Title");
 
         JFrame window = new JFrame();
 
-        window.setTitle("Első ablak");
+        window.setTitle(Title);
         window.setSize(400,300);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 

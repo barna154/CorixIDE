4a8948de1585cd85fea525a71bf2d0b80001db45
barna154 <barnax.craft@gmail.com>
2026-03-10 22:34:58 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -84,7 +84,7 @@ public class Main {
 
 
                 Menu.add(Box.createRigidArea(new Dimension(750, 0)));
-                JLabel label3 = new JLabel(" X  "); 
+                JLabel label3 = new JLabel("  X  "); 
                 label3.setFont(new Font("Arial", Font.PLAIN, 20));
                 label3.setForeground(new Color(118, 118, 118));
                 label3.addMouseListener(new MouseAdapter() {

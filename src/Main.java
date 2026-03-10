785dccd0fe65ca741ef90d1beec4447cd345d94e
barna154 <barnax.craft@gmail.com>
2026-03-10 20:47:28 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,25 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 import org.json.JSONArray;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.json.JSONObject;
 
 public class Main {
-     static String json = Paths.get("lang/lang.json");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        JSONObject obj = new JSONObject(json);
+        // JSON beolvasása
+        String content = new String(Files.readAllBytes(Paths.get("lang/lang.json")));
+        JSONObject obj = new JSONObject(content);
+
         String Title = obj.getString("Title");
 
+        // JFrame létrehozása
         JFrame window = new JFrame();
-
         window.setTitle(Title);
-        window.setSize(400,300);
+        window.setSize(400, 300);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         window.setVisible(true);
     }
 }

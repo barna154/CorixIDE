4d0b0c457aa758997c4256e211841ada455bb9ff
barna154 <barnax.craft@gmail.com>
2026-03-11 14:38:52 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -360,13 +360,7 @@ public class Main {
         console.setBorder(BorderFactory.createEmptyBorder(300, 0, 0, 0));
         console.setPreferredSize(new Dimension(0, 250));
 
-        JPanel line2 = new JPanel();
-        line2.setBackground(new Color(105, 10, 10));
-        line2.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-        line2.setPreferredSize(new Dimension(1, 200));
-
         back.add(explolerp, BorderLayout.WEST);
-        back.add(line2);
         back.add(console, BorderLayout.SOUTH);
         window.add(back, BorderLayout.CENTER);
 

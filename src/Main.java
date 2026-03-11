5666b006480bac80847fbb1f16e14ad399789b8d
barna154 <barnax.craft@gmail.com>
2026-03-11 13:47:02 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -279,7 +279,7 @@ public class Main {
 
         
         JPanel line1 = new JPanel();
-        line1.setBackground(new Color(80, 80, 80, 20));
+        line1.setBackground(new Color(255, 0, 0, 20));
         line1.setPreferredSize(new Dimension(screenWidth, 1));
     
 

c92552984c1b6c712d31f71bf2b84ac529396050
barna154 <barnax.craft@gmail.com>
2026-03-11 14:41:51 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -355,7 +355,7 @@ public class Main {
 
 
         JPanel console = new JPanel();
-        console.setBackground(new Color(20, 20, 20));
+        console.setBackground(new Color(50, 50, 50));
         console.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
         console.setBorder(BorderFactory.createEmptyBorder(300, 0, 0, 0));
         console.setPreferredSize(new Dimension(0, 250));

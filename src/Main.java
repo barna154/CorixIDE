0aca56fcae84701a08e54b4a1574e4500625da92
barna154 <barnax.craft@gmail.com>
2026-03-11 14:08:54 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -298,7 +298,7 @@ public class Main {
         JPanel explolerp = new JPanel();
         explolerp.setBackground(new Color(40, 40, 40));
         explolerp.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-        explolerp.setPreferredSize(new Dimension(200, screenHeight));
+        explolerp.setPreferredSize(new Dimension(250, screenHeight));
                         final int PANEL_RESIZE_MARGIN = 5;
                         final Point[] panelStartMouse = new Point[1];
                         final int[] panelStartWidth = new int[1];

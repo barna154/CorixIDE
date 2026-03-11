5533ac2523b48ab6886dc071c11612f0901ab8dd
barna154 <barnax.craft@gmail.com>
2026-03-11 12:35:52 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -122,6 +122,7 @@ public class Main {
 
         JLabel maximize = new JLabel("🗗");
         maximize.setPreferredSize(new Dimension(45, 43));
+        maximize.setBorder(BorderFactory.createEmptyBorder(2, 0, 0, 0));
         maximize.setFont(new Font("Noto Sans Symbols 2", Font.PLAIN, 22));
         maximize.setHorizontalAlignment(SwingConstants.CENTER);
         maximize.setVerticalAlignment(SwingConstants.CENTER);

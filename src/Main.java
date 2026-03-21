1a4ba8e11c362c5a70f5b1917311927bb029135e
barna154 <barnax.craft@gmail.com>
2026-03-21 08:25:01 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -386,7 +386,7 @@ public class Main {
         back.setLayout(new BorderLayout());
 
         JPanel explolerp = new JPanel();
-        explolerp.setBackground(new Color(0, 41, 11));
+        explolerp.setBackground(new Color(16, 23, 16));
         explolerp.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
         explolerp.setPreferredSize(new Dimension(250, screenHeight));
                         final int PANEL_RESIZE_MARGIN = 5;

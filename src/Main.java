5de87e987ea7caa83478b15433339a34519b3790
barna154 <barnax.craft@gmail.com>
2026-03-21 08:12:39 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -168,7 +168,7 @@ public class Main {
             });
         leftPanel.add(labelBoards);
 
-        JLabel uploadb = new JLabel("📤");
+        JLabel uploadb = new JLabel("⬆️");
         //uploadb.setPreferredSize(new Dimension(45, 43));
         uploadb.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 20));
         uploadb.setHorizontalAlignment(SwingConstants.CENTER);

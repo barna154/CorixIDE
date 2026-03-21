2c9103e16c4121d56ea9a39b04289ffa4957b7e6
barna154 <barnax.craft@gmail.com>
2026-03-21 09:09:51 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -672,11 +672,35 @@ resizeBorder.addMouseListener(new MouseAdapter() {
     }
 });
 
-
-
-
-
         window.setVisible(true);
     }
 
+}
+
+class RoundedBorder implements Border {
+    private int radius;
+    private Color color;
+
+    public RoundedBorder(int radius, Color color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(radius, radius, radius, radius);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return false;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(color);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+    }
 }

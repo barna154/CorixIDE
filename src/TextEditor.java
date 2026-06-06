5528db5d37f19ec3012465c9cb071a7b6ae871c7
barna154 <barnax.craft@gmail.com>
2026-06-06 11:22:09 +0200

    Editor

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -14,9 +14,13 @@ public class TextEditor {
         textArea.setBackground(new Color(30,30,30));
         textArea.setForeground(Color.WHITE);
         textArea.setCaretColor(Color.WHITE);
+        
 
         JScrollPane scroll = new JScrollPane(textArea);
         scroll.setBorder(new EmptyBorder(1, 1, 1, 1));
+        scroll.setBorder(null);
+        scroll.getViewport().setBorder(null);
+        scroll.getViewport().setBackground(new Color(30,30,30));
 
         editorPanel.add(scroll, BorderLayout.CENTER);
     }

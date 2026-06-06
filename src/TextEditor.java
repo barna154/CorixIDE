199a71f33fee1de94ba5b92efe946379712537ea
barna154 <barnax.craft@gmail.com>
2026-06-06 12:40:06 +0200

    Editor

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -8,7 +8,7 @@ public class TextEditor {
         JTextArea textArea = new JTextArea();
 
         textArea.setBackground(new Color(30, 30, 30));
-        textArea.setForeground(Color.WHITE);
+        textArea.setForeground(new Color(218, 218, 218));
         textArea.setCaretColor(Color.GRAY);
         textArea.setFont(new Font("Consolas", Font.PLAIN, 17));
 

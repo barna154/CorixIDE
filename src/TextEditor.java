ec5e7469eb8d82569ccaeade0a1e3913706d867d
barna154 <barnax.craft@gmail.com>
2026-06-06 14:35:11 +0200

    Scrollbar

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -20,7 +20,7 @@ public class TextEditor {
         scroll.getVerticalScrollBar().setUI(new CustomScrollBarUI());
         scroll.getHorizontalScrollBar().setUI(new CustomScrollBarUI());
         scroll.getVerticalScrollBar().setPreferredSize(new Dimension(18, 0));
-        scroll.getHorizontalScrollBar().setPreferredSize(new Dimension(0, 0.3));
+        scroll.getHorizontalScrollBar().setPreferredSize(new Dimension(0, 100));
 
 
         editorPanel.setLayout(new BorderLayout());

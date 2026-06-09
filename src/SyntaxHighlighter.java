7bef2ccd9e2eb7a8cfc6af82e96f138771ce7672
barna154 <barnax.craft@gmail.com>
2026-06-09 18:36:25 +0200

    highlightcolors

--- src/SyntaxHighlighter.java
+++ src/SyntaxHighlighter.java
@@ -24,13 +24,14 @@ public class SyntaxHighlighter {
 
     private Style numberStyle;
     private Style normalStyle;
+    private Style tesztStyle;
 
     private void createStyles() {
         normalStyle = doc.addStyle("normal", null);
         StyleConstants.setForeground(normalStyle, new Color(218, 218, 218));
 
         teszt = doc.addStyle("teszt", null);
-        StyleConstants.setForeground(teszt, new Color(255, 120, 120));
+        StyleConstants.setForeground(tesztStyle, new Color(255, 120, 120));
 
         numberStyle = doc.addStyle("number", null);
         StyleConstants.setForeground(numberStyle, new Color(75, 220, 165));
@@ -65,7 +66,7 @@ public class SyntaxHighlighter {
 
             Matcher teszt = Pattern.compile("\\bteszt\\b").matcher(text);
             while (teszt.find()) {
-                doc.setCharacterAttributes(teszt.start(), teszt.end() - teszt.start(), teszt, false);
+                doc.setCharacterAttributes(teszt.start(), teszt.end() - teszt.start(), tesztStyle, false);
             }
 
         } catch (Exception ex) {

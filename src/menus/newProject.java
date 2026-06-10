8cbc6ad0f0d8c4970f25f6bf2918535d52f753bb
barna154 <barnax.craft@gmail.com>
2026-06-11 00:53:54 +0200

    newprojectmenu

--- src/menus/newProject.java
+++ src/menus/newProject.java
@@ -77,7 +77,7 @@ public class newProject {
         Menu.add(rightPanel, BorderLayout.EAST);
         newProject.add(Menu, BorderLayout.NORTH);
 
-        JPanel rightPanel2 = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 10));
+        JPanel rightPanel2 = new JPanel(new FlowLayout(FlowLayout.RIGHT, 20, 20));
         rightPanel2.setOpaque(false);
         JLabel finishBtn = new JLabel("Finish");
         finishBtn.setPreferredSize(new Dimension(90, 30));

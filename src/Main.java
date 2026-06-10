589a9c6c811c5e7b8a33cee7b732a960e3d6cb64
barna154 <barnax.craft@gmail.com>
2026-06-10 21:48:21 +0200

    FileExplorer

--- src/Main.java
+++ src/Main.java
@@ -491,7 +491,7 @@ public class Main {
 
         FileExplorer fe = new FileExplorer();
         fe.init(explolerp);
-        explolerp.add(sourcecon, BorderLayout.CENTER);
+        explolerp.add(sourcecon, BorderLayout.LEFT);
         
         
 

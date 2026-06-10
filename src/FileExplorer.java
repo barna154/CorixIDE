d61e7ef33b731822a705fa2891a22e29d19b073c
barna154 <barnax.craft@gmail.com>
2026-06-10 21:51:42 +0200

    FileExplorer

--- src/FileExplorer.java
+++ src/FileExplorer.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class FileExplorer {
 
-    public void init(JPanel filePanel) {
+    public void init(JPanel filePanel String[] args) throws Exception {
         LanguageManager.load("../lang/lang.json");
         
         String sourcecont = LanguageManager.get("Explorer");

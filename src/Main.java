4c2d3aa6637cf3f6ea7c51125a7c5ca5cbe3a507
barna154 <barnax.craft@gmail.com>
2026-06-10 22:28:40 +0200

    newprojectmenu

--- src/Main.java
+++ src/Main.java
@@ -600,10 +600,10 @@ public class Main {
 
                     JPanel newProjectPanel = new JPanel();
                     newProjectPanel.setBackground(new Color(50, 50, 50));
-                    double panelWidth = screenWidth/1.5;
-                    double panelHeight = screenHeight/1.5;
-                    double x = (screenWidth - panelWidth) / 2;
-                    double y = (screenHeight - panelHeight) / 2;
+                    int panelWidth = (int) (screenWidth / 1.5);
+                    int panelHeight = (int) (screenHeight / 1.5);
+                    int x = (screenWidth - panelWidth) / 2;
+                    int y = (screenHeight - panelHeight) / 2;
 
                     newProjectPanel.setBounds(x, y, panelWidth, panelHeight);
                     newProjectPanel.setLayout(new BorderLayout());

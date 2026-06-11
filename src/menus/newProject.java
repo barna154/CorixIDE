fb8394f7dac209de2996cc39190ef66b8d9af6fb
barna154 <barnax.craft@gmail.com>
2026-06-11 12:56:29 +0200

    newprojectmenu

--- src/menus/newProject.java
+++ src/menus/newProject.java
@@ -80,14 +80,14 @@ public class newProject {
         newProject.add(Menu, BorderLayout.NORTH);
 
 
-        JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JPanel centerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 30));
         centerPanel.setBackground(new Color(140, 140, 140));
         JLabel namePanel = new JLabel(npan);
         namePanel.setFont(new Font("Segoe UI Emoji", Font.BOLD, 16));
         namePanel.setForeground(new Color(50, 50, 50));
         JTextField pathField = new JTextField(40);
         pathField.setBackground(new Color(200, 200, 200));
-        pathField.setBorder(BorderFactory.createLineBorder(new Color(20, 20, 20), 2));
+        pathField.setBorder(BorderFactory.createLineBorder(new Color(20, 20, 20), 3));
         pathField.setFont(new Font("Segoe UI Emoji", Font.BOLD, 16));
         String projectPath = pathField.getText();
 

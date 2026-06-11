e785f448e248f89a7aa3931f8064707a5b098e0b
barna154 <barnax.craft@gmail.com>
2026-06-11 13:00:10 +0200

    newprojectmenu

--- src/menus/newProject.java
+++ src/menus/newProject.java
@@ -87,7 +87,6 @@ public class newProject {
         namePanel.setForeground(new Color(50, 50, 50));
         JTextField pathField = new JTextField(40);
         pathField.setLineWrap(false);
-        pathField.setAlignmentY(Component.CENTER_ALIGNMENT);
         pathField.setBackground(new Color(200, 200, 200));
         pathField.setBorder(BorderFactory.createLineBorder(new Color(20, 20, 20), 3));
         pathField.setFont(new Font("Segoe UI Emoji", Font.BOLD, 16));

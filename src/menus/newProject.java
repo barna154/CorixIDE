d98078c749b6c0e373194e603fed65a09e3e2a93
barna154 <barnax.craft@gmail.com>
2026-06-11 12:54:37 +0200

    newprojectmenu

--- src/menus/newProject.java
+++ src/menus/newProject.java
@@ -87,6 +87,7 @@ public class newProject {
         namePanel.setForeground(new Color(50, 50, 50));
         JTextField pathField = new JTextField(40);
         pathField.setBackground(new Color(200, 200, 200));
+        pathField.setBorder(BorderFactory.createLineBorder(new Color(20, 20, 20), 2));
         String projectPath = pathField.getText();
 
         centerPanel.add(namePanel);

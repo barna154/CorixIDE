0f82c4f36c108e9c87ddb916b84051c8cd463d36
barna154 <barnax.craft@gmail.com>
2026-06-13 20:24:18 +0200

    FileExplorer

--- src/FileExplorer.java
+++ src/FileExplorer.java
@@ -136,7 +136,7 @@ public class FileExplorer {
         scrollPane.getHorizontalScrollBar().setUI(new CustomScrollBarUI());
 
         scrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(8, 0));
-        scrollPane.getHorizontalScrollBar().setPreferredSize(new Dimension(0, 8));
+        scrollPane.getHorizontalScrollBar().setPreferredSize(new Dimension(8, 8));
 
         filePanel.add(scrollPane, BorderLayout.CENTER);
 

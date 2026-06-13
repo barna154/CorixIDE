1b9bcef61393f36f5679a14884f5fdde79cbd429
barna154 <barnax.craft@gmail.com>
2026-06-13 18:05:00 +0200

    FileExplorer

--- src/FileExplorer.java
+++ src/FileExplorer.java
@@ -111,7 +111,7 @@ public class FileExplorer {
                             setForeground(new Color(129, 153, 132));
                     } else if (file.getName().endsWith(".crxprjct")) {
                             String name = file.getName().replaceFirst("\\.crxprjct$", "");
-                            setText("⚙ " + name);
+                            setText("<html><span style='font-size:12px'>⚙</span> " +  "<span style='font-size:15px'>" + name + "</span></html>");
                             setForeground(new Color(49, 224, 71));
                     } else {
                             setText("📄 " + file.getName());

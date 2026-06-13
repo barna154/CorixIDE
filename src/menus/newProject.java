7f66ffe086504952c4080c45e09cf35ac87a1c52
barna154 <barnax.craft@gmail.com>
2026-06-13 12:28:24 +0200

    newprojectmenu

--- src/menus/newProject.java
+++ src/menus/newProject.java
@@ -83,7 +83,7 @@ public class newProject {
 
         JPanel centerPanel = new JPanel();
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS)); 
-        centerPanel.setBackground(new Color(170, 170, 170));
+        centerPanel.setBackground(new Color(30, 33, 30));
 
 
         JPanel row1 = new JPanel(new FlowLayout(FlowLayout.LEFT));

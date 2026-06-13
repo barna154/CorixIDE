1d1052e7f9d580ea09eeb4e43719a1270001ac18
barna154 <barnax.craft@gmail.com>
2026-06-13 22:33:06 +0200

    TextEditor

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.charset.StandardCharsets;
 import util.LanguageManager;
+import util.KeyStrokes;
 
 public class TextEditor {
 

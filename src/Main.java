5485a2d34d15f0424d00fd3a35c7870284a48899
barna154 <barnax.craft@gmail.com>
2026-06-13 17:16:19 +0200

    FileExplorer

--- src/Main.java
+++ src/Main.java
@@ -492,7 +492,7 @@ public class Main {
         folderMenu.add(newFileInFolder);
         folderMenu.add(renameFolder);
         folderMenu.add(deleteFolder);
-        dataexplorer.add(sep);
+        folderMenu.add(sep);
         folderMenu.add(mainpr);
 
 

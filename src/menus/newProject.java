5231f947869faf1568a6a4e2b5e9482b09fdbe80
barna154 <barnax.craft@gmail.com>
2026-06-13 13:30:02 +0200

    newprojectmenu

--- src/menus/newProject.java
+++ src/menus/newProject.java
@@ -158,7 +158,7 @@ public class newProject {
                 }
                 @Override
                 protected JButton createArrowButton() {
-                    JButton button = new JButton("V");
+                    JButton button = new JButton("⇩");
                     button.setBackground(new Color(30, 30, 30));
                     button.setForeground(new Color(230, 230, 230));
                     button.setBorder(BorderFactory.createEmptyBorder());

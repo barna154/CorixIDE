a715009725b1a52ccad3cba81b5e5e6df9316d60
barna154 <barnax.craft@gmail.com>
2026-06-13 13:00:19 +0200

    newprojectmenu

--- src/menus/newProject.java
+++ src/menus/newProject.java
@@ -89,6 +89,7 @@ public class newProject {
         JPanel row1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
         row1.setPreferredSize(new Dimension(newProject.getWidth(), 40));
         row1.setMaximumSize(row1.getPreferredSize());
+        row1.setBackground(centerPanel.getBackground());
 
 
         JLabel namePanel = new JLabel(npan);
@@ -104,7 +105,7 @@ public class newProject {
 
         JPanel row2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
         row2.setPreferredSize(new Dimension(newProject.getWidth(), 40));
-        row2.setBackground(new Color(70, 77, 70));
+        row2.setBackground(centerPanel.getBackground());
         row2.setMaximumSize(row2.getPreferredSize());
         JLabel mconPanel = new JLabel(mcon);
         mconPanel.setFont(new Font("Segoe UI Emoji", Font.BOLD, 16));

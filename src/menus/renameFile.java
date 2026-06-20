b1f4a3bb3a482e171a3dfd430db834de0c001aae
barna154 <barnax.craft@gmail.com>
2026-06-20 21:52:23 +0200

    rename

--- src/menus/renameFile.java
+++ src/menus/renameFile.java
@@ -95,7 +95,7 @@ public class renameFile {
 
         JLabel namePanel = new JLabel(npan);
         namePanel.setFont(new Font("Segoe UI Emoji", Font.BOLD, 16));
-        namePanel.setPreferredSize(new Dimension(125, 25));
+        namePanel.setPreferredSize(new Dimension(85, 25));
         namePanel.setForeground(new Color(225, 225, 225));
         JTextField pathField = new JTextField(20);
         pathField.setBackground(new Color(30, 30, 30));

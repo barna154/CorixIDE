5f6db08879a881f10c40a049b2adcaa3be5cf5d5
barna154 <barnax.craft@gmail.com>
2026-06-22 15:09:01 +0200

    boardBtn

--- src/menus/boardSelector.java
+++ src/menus/boardSelector.java
@@ -36,7 +36,7 @@ public class boardSelector {
 
 
                 JMenuItem item1 = new JMenuItem("PIC16F15256");
-                    if item1.getName().equals(selectedBoard.BoardName) {
+                    if (item1.getName().equals(selectedBoard.BoardName)) {
                         item1.setForeground(new Color(13, 255, 122));
                     } else {
                         item1.setForeground(new Color(230, 230, 230));
@@ -46,7 +46,7 @@ public class boardSelector {
                     item1.setFont(new Font("Arial", Font.PLAIN, 15));
                     item1.setOpaque(true);
                 JMenuItem item2 = new JMenuItem("PIC16F15274");
-                    if item1.getName().equals(selectedBoard.BoardName) {
+                    if (item1.getName().equals(selectedBoard.BoardName)) {
                         item1.setForeground(new Color(13, 255, 122));
                     } else {
                         item1.setForeground(new Color(230, 230, 230));
@@ -57,7 +57,7 @@ public class boardSelector {
                     item2.setForeground(new Color(230, 230, 230));
                     item2.setOpaque(true);
                 JMenuItem item3 = new JMenuItem("PIC16F15275");
-                    if item1.getName().equals(selectedBoard.BoardName) {
+                    if (item1.getName().equals(selectedBoard.BoardName)) {
                         item1.setForeground(new Color(13, 255, 122));
                     } else {
                         item1.setForeground(new Color(230, 230, 230));
@@ -68,7 +68,7 @@ public class boardSelector {
                     item3.setForeground(new Color(230, 230, 230));
                     item3.setOpaque(true);
                 JMenuItem item4 = new JMenuItem("PIC16F15276");
-                    if item1.getName().equals(selectedBoard.BoardName) {
+                    if (item1.getName().equals(selectedBoard.BoardName)) {
                         item1.setForeground(new Color(13, 255, 122));
                     } else {
                         item1.setForeground(new Color(230, 230, 230));
@@ -79,7 +79,7 @@ public class boardSelector {
                     item4.setForeground(new Color(230, 230, 230));
                     item4.setOpaque(true);
                 JMenuItem item5 = new JMenuItem("Atmega328P");
-                    if item1.getName().equals(selectedBoard.BoardName) {
+                    if (item1.getName().equals(selectedBoard.BoardName)) {
                         item1.setForeground(new Color(13, 255, 122));
                     } else {
                         item1.setForeground(new Color(230, 230, 230));

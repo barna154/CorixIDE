b91ba9c47a95f80facc8bb26afc46f259f4b13a4
barna154 <barnax.craft@gmail.com>
2026-06-22 14:57:35 +0200

    boardBtn

--- src/FileExplorer.java
+++ src/FileExplorer.java
@@ -120,9 +120,7 @@ public class FileExplorer {
                             String name = file.getName().replaceFirst("\\.crxprjct$", "");
                             setIcon(new EmojiIcon("⚙", new Color(0x6F, 0xA1, 0x18), 15));
                             setText(name);
-                        } else if (file.getName().endsWith(".hex")) {
-
-                        }else {
+                        } else {
                             setIcon(new EmojiIcon("📄", new Color(0xA4, 0xE0, 0xAC), 15));
                             setText(file.getName());
                         }

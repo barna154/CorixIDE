e61b21d08b25c9007b4754d6bec4412195ae54cf
barna154 <barnax.craft@gmail.com>
2026-06-25 16:12:57 +0200

    findandreplace

--- src/Main.java
+++ src/Main.java
@@ -485,8 +485,8 @@ public class Main {
                     JPanel findReplacePanel = new JPanel();
                     findReplacePanel.setBackground(new Color(50, 50, 50));
                     findReplacePanel.setBorder(BorderFactory.createLineBorder(new Color(100, 100, 100), 1));
-                    int panelWidth4 = (int) (screenWidth / 4.5);
-                    int panelHeight4 = (int) (screenHeight / 7);
+                    int panelWidth4 = (int) (screenWidth / 5.5);
+                    int panelHeight4 = (int) (screenHeight / 5.5);
                     int x4 = (int) (screenWidth - panelWidth3);
                     int y4 = (screenHeight - panelHeight3) / 8;
 

8623013d08a02a4fc0b91b7b5f0a942aeafc7f7e
barna154 <barnax.craft@gmail.com>
2026-07-25 17:51:48 +0200

    complier

--- src/compliers/pic16F1527x.java
+++ src/compliers/pic16F1527x.java
@@ -130,10 +130,7 @@ public class pic16F1527x {
                 }
             console.println("----------------");
 
-            console.println("Bool változók címei:");
-            for (Map.Entry<String, Integer> entry : boolAddresses.entrySet()) {
-                console.println(entry.getKey() + " -> 0x" + Integer.toHexString(entry.getValue()));
-            }
+         
             console.println("----------------");
 
 

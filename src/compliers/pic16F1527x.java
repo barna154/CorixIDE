e48cb2d375120542c080e6ebbfa5c351630a6d0e
barna154 <barnax.craft@gmail.com>
2026-07-25 14:39:13 +0200

    complier

--- src/compliers/pic16F1527x.java
+++ src/compliers/pic16F1527x.java
@@ -117,12 +117,14 @@ public class pic16F1527x {
             console.println("----------------");
 
 
-            String confighex = "0A" + config1 + config2 + config3 + config4 + config5;
+            String confighex = "0A000E00" + config1 + config2 + config3 + config4 + config5;
             String checksumconfig = calculateChecksum(confighex);
             String fullconfig = ":" + confighex + checksumconfig;
+            String j16to32 = ":020000040001F9";
+            String eof = ":00000001FF";
 
             String nl = System.lineSeparator();
-            writeOutputFile("hex", fullconfig);
+            writeOutputFile("hex", j16to32 + nl + fullconfig + nl + eof);
         }
 
     private String getCpu(String content) {

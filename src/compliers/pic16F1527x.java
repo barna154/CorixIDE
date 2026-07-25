4201d6a84048b1ce665e0c3398871f22d10af29a
barna154 <barnax.craft@gmail.com>
2026-07-25 20:06:29 +0200

    complier

--- src/compliers/pic16F1527x.java
+++ src/compliers/pic16F1527x.java
@@ -322,9 +322,7 @@ public class pic16F1527x {
 
             if (value.equals("TRUE")) {
                 asm = String.format("BANKSEL 0x%02X "+nl+"     BSF 0x%02X, 0",
-                        address, address);
-                String binaryAddress = String.format("%8s", Integer.toBinaryString(address)).replace(' ', '0');
-                        console.println("Cím binárisan: " + binaryAddress);        
+                        address, address);       
             } else if (value.equals("FALSE")) {
                 asm = String.format("BANKSEL 0x%02X"+nl+"     CLRF 0x%02X",
                         address, address);

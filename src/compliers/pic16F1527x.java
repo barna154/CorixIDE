1241ae034e4136512fe6ca3ebb8a38a9d8819e86
barna154 <barnax.craft@gmail.com>
2026-07-25 14:53:04 +0200

    complier

--- src/compliers/pic16F1527x.java
+++ src/compliers/pic16F1527x.java
@@ -117,9 +117,13 @@ public class pic16F1527x {
             console.println("----------------");
 
 
-            String confighex = "0A000E00" + config1 + config2 + config3 + config4 + config5;
+
+            String commandstart: ":0A";
+
+
+            String confighex = commandstart "000E00" + config1 + config2 + config3 + config4 + config5;
             String checksumconfig = calculateChecksum(confighex);
-            String fullconfig = ":" + confighex + checksumconfig;
+            String fullconfig = confighex + checksumconfig;
             String j16to32 = ":020000040001F9";
             String eof = ":00000001FF";
 

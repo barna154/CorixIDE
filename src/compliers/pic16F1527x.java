721f16d118024fde4ef871e2a7a3a0bd55afca3f
barna154 <barnax.craft@gmail.com>
2026-07-30 16:33:36 +0200

    complier

--- src/compliers/pic16F1527x.java
+++ src/compliers/pic16F1527x.java
@@ -219,7 +219,7 @@ public class pic16F1527x {
 
                 console.println("LOOP utasítások:");
                 int LoopAdress = PROGRAM_MEMORY_START;
-                console.println(LoopAdress);
+                console.println(LoopAdress.toString());
                 codelbuilder.append(":"
                                 + linec
                                 + System.lineSeparator()
